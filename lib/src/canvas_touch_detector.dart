229e7b2943d0274c84f64fdb923ce537fd384bbd
Hwan-seok <tttkhs@naver.com>
2021-12-05 00:48:27 +0900

    feat: enable nested tap event

--- lib/src/canvas_touch_detector.dart
+++ lib/src/canvas_touch_detector.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:touchable/src/types/types.dart';
 
@@ -16,6 +17,13 @@ class CanvasTouchDetector extends StatefulWidget {
   _CanvasTouchDetectorState createState() => _CanvasTouchDetectorState();
 }
 
+class AllowMultiGestureRecognizer extends TapGestureRecognizer {
+  @override
+  void rejectGesture(int pointer) {
+    acceptGesture(pointer);
+  }
+}
+
 class _CanvasTouchDetectorState extends State<CanvasTouchDetector> {
   final StreamController<Gesture> touchController = StreamController.broadcast();
   StreamSubscription? streamSubscription;
@@ -27,81 +35,35 @@ class _CanvasTouchDetectorState extends State<CanvasTouchDetector> {
 
   @override
   Widget build(BuildContext context) {
-    return TouchDetectionController(touchController, addStreamListener,
-        child: GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          child: Builder(
-            builder: (context) {
-              return widget.builder(context);
+    return TouchDetectionController(
+      touchController,
+      addStreamListener,
+      child: RawGestureDetector(
+        behavior: HitTestBehavior.translucent,
+        child: Builder(
+          builder: (context) {
+            return widget.builder(context);
+          },
+        ),
+        gestures: {
+          AllowMultiGestureRecognizer:
+              GestureRecognizerFactoryWithHandlers<AllowMultiGestureRecognizer>(
+            () => AllowMultiGestureRecognizer(),
+            (AllowMultiGestureRecognizer instance) {
+              instance
+                ..onTapUp = (tapDetail) {
+                  touchController.add(
+                    Gesture(GestureType.onTapUp, tapDetail),
+                  );
+                }
+                ..onTapDown = (tapDetail) {
+                  touchController.add(Gesture(GestureType.onTapDown, tapDetail));
+                };
             },
-          ),
-          onTapDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onTapDown, tapDetail));
-          },
-          onTapUp: (tapDetail) {
-            touchController.add(Gesture(GestureType.onTapUp, tapDetail));
-          },
-//        onHorizontalDragDown: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onHorizontalDragDown, tapDetail));
-//        },
-//        onHorizontalDragStart: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onHorizontalDragStart, tapDetail));
-//        },
-//        onHorizontalDragUpdate: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onHorizontalDragUpdate, tapDetail));
-//        },
-//        onVerticalDragDown: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onVerticalDragDown, tapDetail));
-//        },
-//        onVerticalDragStart: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onVerticalDragStart, tapDetail));
-//        },
-//        onVerticalDragUpdate: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onVerticalDragUpdate, tapDetail));
-//        },
-          onLongPressStart: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressStart, tapDetail));
-          },
-          onLongPressEnd: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressEnd, tapDetail));
-          },
-          onLongPressMoveUpdate: (tapDetail) {
-            touchController.add(Gesture(GestureType.onLongPressMoveUpdate, tapDetail));
-          },
-//        onScaleStart: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onScaleStart, tapDetail));
-//        },
-//        onScaleUpdate: (tapDetail) {
-//          touchController.add(Gesture( GestureType.onScaleUpdate, tapDetail));
-//        },
-          onForcePressStart: (tapDetail) {
-            touchController.add(Gesture(GestureType.onForcePressStart, tapDetail));
-          },
-          onForcePressEnd: (tapDetail) {
-            touchController.add(Gesture(GestureType.onForcePressEnd, tapDetail));
-          },
-          onForcePressPeak: (tapDetail) {
-            touchController.add(Gesture(GestureType.onForcePressPeak, tapDetail));
-          },
-          onForcePressUpdate: (tapDetail) {
-            touchController.add(Gesture(GestureType.onForcePressUpdate, tapDetail));
-          },
-          onPanStart: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanStart, tapDetail));
-          },
-          onPanUpdate: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanUpdate, tapDetail));
-          },
-          onPanDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onPanDown, tapDetail));
-          },
-          onSecondaryTapDown: (tapDetail) {
-            touchController.add(Gesture(GestureType.onSecondaryTapDown, tapDetail));
-          },
-          onSecondaryTapUp: (tapDetail) {
-            touchController.add(Gesture(GestureType.onSecondaryTapUp, tapDetail));
-          },
-        ));
+          )
+        },
+      ),
+    );
   }
 
   @override
@@ -119,7 +81,8 @@ class TouchDetectionController extends InheritedWidget {
 
   StreamController<Gesture> get controller => _controller;
 
-  const TouchDetectionController(this._controller, this.addListener, {required Widget child}) : super(child: child);
+  const TouchDetectionController(this._controller, this.addListener, {required Widget child})
+      : super(child: child);
 
   static TouchDetectionController? of(BuildContext context) =>
       context.dependOnInheritedWidgetOfExactType<TouchDetectionController>();
